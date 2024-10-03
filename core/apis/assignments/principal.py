4afb42257b423245cfe693d4797b01e3b54db6a3
rythm-sachdeva <rythmrko123@gmail.com>
2024-10-03 17:51:56 +0530

    Implemented endpoint where principal can grade assignments.

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -3,7 +3,7 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Teacher, Assignment
-from .schema import TeacherSchema , AssignmentSchema
+from .schema import TeacherSchema , AssignmentSchema,AssignmentGradeSchema
 
 principal_resources = Blueprint('principal_resources',__name__)
 
@@ -20,4 +20,23 @@ def list_assignments(p):
     all_assignments = Assignment.get_assignments_by_teacher()
     all_assignments_dump = AssignmentSchema().dump(all_assignments, many=True)
     return APIResponse.respond(data=all_assignments_dump)
+
+
+@principal_resources.route('/assignments/grade',methods=['POST'],strict_slashes=True)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def regrade_assignments(p,incoming_payload):
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
+
+
+
 
